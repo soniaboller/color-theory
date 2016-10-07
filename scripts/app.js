2efc3bfc197435a3c411005694e9bdbd97a063ce
Sonia Boller <soniaboller@Sonias-MacBook-Pro.local>
2016-10-06 21:01:30 -0500

    added function that determines whether tiles to the left and right of the clicked tile are lighter than it

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,12 @@
-// timer hits zero end game
+// timer hits zero end level
 // if time hits zero and score is below x number you lose
 // if timer hits zero and score is above number or move on
 // before game starts show color to compare darker or lighter -- maybe keep this at the top of the screen? or somewhere in the screen?
 // as the game progresses make the color range smaller so it gets harder
 
+// create a div to append boxes to rather than the body -- multiple divs to allow for horizontal sliding??
+
+// maybe do purple and other mixed colors if time permits?
 
 $(document).ready(function(){
     console.log("linked");
@@ -16,6 +19,26 @@ $(document).ready(function(){
 var colorButtonChoice = '';
 var score = 0;
 var time = 25;
+
+
+// GAMEPLAY FUNCTIONS
+function addScore(){
+    score += 1;
+    $('#score-div').html('SCORE : ' + score);
+}
+
+function subtractScore(){
+    score -= 3;
+    $('#score-div').html('SCORE : ' + score);
+}
+
+function rgbToArray(colorToConvert){
+    var colorArray = [];
+    var color = colorToConvert.substring(3).replace('(', '').replace(')', ''); // cuts off the rgb part of color tag
+    colorArray.push(color.split(', ')); // removes the commas and pushes into color array
+    console.log('this is the color array '+ colorArray);
+    return colorArray;
+}
 
 
 // ON CLICK FUNCTIONS
@@ -38,61 +61,89 @@ $('#start').on('click', function(){
         // boxes fading in at start
     });
     $('.box').on('click', function(){
-        var id = this.id;
-        console.log('this is the '+ id);
-        var clickedColor = $('#'+id).css('backgroundColor'); //logs rgb value of clicked colors
-        // check rgb value at blue and if it is lower than a certain number console log something
-        var colorArray = [];
-        var color = clickedColor.substring(3).replace('(', '').replace(')', ''); // cuts off the rgb part of color tag
-        colorArray.push(color.split(', ')); // removes the commas and pushes into color array
-        console.log(colorArray);
+        var id = this.id; // id of box clicked
+        var nextBox = $('#'+id).next();
+        var previousBox = $('#'+id).prev();
+        var clickedColor = $('#'+id).css('backgroundColor'); // stores rgb value of clicked color
+        var nextBoxColor = nextBox.css('backgroundColor'); // stores rgb value of next color
+        var previousBoxColor = previousBox.css('backgroundColor'); // stores rgb value of previous color
+        var clickedColorArray = rgbToArray(clickedColor); // creates array for clicked color
+        var nextBoxColorArray = rgbToArray(nextBoxColor); // creates array for next color
+        var previousBoxColorArray = rgbToArray(previousBoxColor); // creates array for previous color
+        console.log('this is the clicked box id '+ id); // clicked box ID
+        console.log('this is the next box id '+ nextBox[0].id); // next box ID
+        console.log('this is the previous box id '+ previousBox[0].id); // previous box ID
+        console.log('this is the clicked color '+ clickedColor); // rgb of clicked color
+        console.log('this is the next box color '+ nextBoxColor); // rgb of next color
+        console.log('this is the previous box color '+ previousBoxColor); // rgb of prev color
+        console.log('clicked box color array '+ clickedColorArray); //rgb array of clicked color
+        console.log('next box color array '+ nextBoxColorArray); //rgb array of next color
+        console.log('previous box color array '+ previousBoxColorArray); //rgb array of prev color
         if(colorButtonChoice === 'blue'){
-            if(colorArray[0][2] < 100){
-                $('#'+id).text('dark blue color');
-                console.log('dark blue color');
-                score += 1;
-                $('#score-div').html('SCORE : ' + score);
+            var clickedBlue = parseInt(clickedColorArray[0][2]);
+            var nextBlue = parseInt(nextBoxColorArray[0][2]);
+            var previousBlue = parseInt(previousBoxColorArray[0][2]);
+
+            console.log('clicked '+ clickedBlue); // blue value of clicked color array
+            console.log('next '+ nextBlue); // blue value of next color array
+            console.log('prev '+ previousBlue); // blue value of prev color array
+
+            if(clickedBlue < nextBlue && clickedBlue < previousBlue){
+                $('#'+id).text('darker');
+                console.log('darker (blue)');
+                addScore();
             }
             else {
-                $('#'+id).text('not dark blue enough!!');
-                console.log('not dark blue enough!!');
-                score -= 1;
-                $('#score-div').html('SCORE : ' + score);
+                $('#'+id).text('lighter');
+                console.log('lighter (blue)');
+                subtractScore();
             }
         }
         else if (colorButtonChoice === 'green'){
-            if(colorArray[0][1] < 100){
-                $('#'+id).text('dark green color');
-                console.log('dark green color');
-                score += 1;
-                $('#score-div').html('SCORE : ' + score);
+            var clickedGreen = parseInt(clickedColorArray[0][1]);
+            var nextGreen = parseInt(nextBoxColorArray[0][1]);
+            var previousGreen = parseInt(previousBoxColorArray[0][1]);
+
+            console.log('clicked '+ clickedGreen); // green value of clicked color array
+            console.log('next '+ nextGreen); // green value of next color array
+            console.log('prev '+ previousGreen); // green value of prev color array
+
+            if(clickedGreen < nextGreen && clickedGreen < previousGreen){
+                $('#'+id).text('darker');
+                console.log('darker (green)');
+                addScore();
             }
             else {
-                $('#'+id).text('not dark green enough!!');
-                console.log('not dark green enough!!');
-                score -= 1;
-                $('#score-div').html('SCORE : ' + score);
+                $('#'+id).text('lighter');
+                console.log('lighter (green)');
+                subtractScore();
             }
         }
         else if (colorButtonChoice === 'red'){
-            if(colorArray[0][0] < 100){
-                $('#'+id).text('dark red color');
-                console.log('dark red color');
-                score += 1;
-                $('#score-div').html('SCORE : ' + score);
+            var clickedRed = parseInt(clickedColorArray[0][0]);
+            var nextRed = parseInt(nextBoxColorArray[0][0]);
+            var previousRed = parseInt(previousBoxColorArray[0][0]);
+
+            console.log('clicked '+ clickedRed); // red value of clicked color array
+            console.log('next '+ nextRed); // red value of next color array
+            console.log('prev '+ previousRed); // red value of prev color array
+
+            if(clickedRed < nextRed && clickedRed < previousRed){
+                $('#'+id).text('darker');
+                console.log('darker (red)');
+                addScore();
             }
             else {
-                $('#'+id).text('not dark red enough!!');
-                console.log('not dark red enough!!');
-                score -= 1;
-                $('#score-div').html('SCORE : ' + score);
+                $('#'+id).text('lighter');
+                console.log('lighter (red)');
+                subtractScore();
             }
         }
         else {
             console.log('you probably tried to add the purple button back in and there is no if else statement for that yet');
         }
-        console.log(clickedColor);
         $('#'+id).fadeOut(500, function(){
+            this.remove();
             // boxes fading out on click
         })
     });
@@ -110,7 +161,11 @@ $('.color-button').on('click', function(){
     }
     else if (colorId === 'purple'){
         colorButtonChoice =  'purple';
-        console.log('red works!');
+        console.log('purple works!');
+    }
+    else if (colorId === 'teal'){
+        colorButtonChoice =  'teal';
+        console.log('teal works!');
     }
     else {
         colorButtonChoice =  'green';
@@ -129,7 +184,7 @@ var createBoard = function(){
     $('body').prepend('<div id="time-div"></div>');
     $('#score-div').text('SCORE : ' + score);
     $('#time-div').text('time remaining : ' + time);
-    for (var i = 1; i <= 36; i++) {
+    for (var i = 1; i <= 100; i++) {
         var $newdiv = $('<div class="box"/>');
         if (colorButtonChoice === "blue"){
             var colorBlue = randomRGBBlue();
@@ -149,6 +204,12 @@ var createBoard = function(){
             $($newdiv).css('background-color',colorPurple);
             $($newdiv).prop('id',i);
         }
+        else if (colorButtonChoice === "teal"){
+            var colorTeal = randomRGBTeal();
+            $('body').append($newdiv);
+            $($newdiv).css('background-color',colorTeal);
+            $($newdiv).prop('id',i);
+        }
         else{
             var colorGreen = randomRGBGreen();
             $('body').append($newdiv);
@@ -165,7 +226,6 @@ $('body').keydown(function(e){
         console.log('refresh');
     }
 });
-
 
 
 // set interval to keep creating colors but hide overflow?
@@ -196,6 +256,10 @@ function randomRGBGreen(){
 
 function randomRGBPurple(){
     return 'rgb(' + randomMid() + ', ' + 0 + ', ' + randomMid() + ')';
+}
+
+function randomRGBTeal(){
+    return 'rgb(' + 0 + ', ' + randomMid() + ', ' + randomMid() + ')';
 }
 
 function randomRGBColor() {
